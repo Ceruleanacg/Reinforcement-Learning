97d8c09f7fd5a3dee77bbd8e90fcbda76cdf0d39
Shuyu <ceruleanacg@gmail.com>
2018-01-31 16:22:01 +0900

    modifiy pg

--- playground/playground.py
+++ playground/playground.py
@@ -1,165 +1,174 @@
-import numpy as np
+"""
+Actor-Critic with continuous action using TD-error as the Advantage, Reinforcement Learning.
+The Pendulum example (based on https://github.com/dennybritz/reinforcement-learning/blob/master/PolicyGradient/Continuous%20MountainCar%20Actor%20Critic%20Solution.ipynb)
+Cannot converge!!! oscillate!!!
+View more on my tutorial page: https://morvanzhou.github.io/tutorials/
+Using:
+tensorflow r1.3
+gym 0.8.0
+"""
+
 import tensorflow as tf
+import numpy as np
 import gym
-import matplotlib.pyplot as plt
 
-# reproducible
-np.random.seed(1)
-tf.set_random_seed(1)
+np.random.seed(2)
+tf.set_random_seed(2)  # reproducible
 
 
-class PolicyGradient:
-    def __init__(
-            self,
-            n_actions,
-            n_features,
-            learning_rate=0.01,
-            reward_decay=0.95,
-            output_graph=False,
-    ):
-        self.n_actions = n_actions
-        self.n_features = n_features
-        self.lr = learning_rate
-        self.gamma = reward_decay
+class Actor(object):
+    def __init__(self, sess, n_features, action_bound, lr=0.0001):
+        self.sess = sess
 
-        self.ep_obs, self.ep_as, self.ep_rs = [], [], []
+        self.s = tf.placeholder(tf.float32, [1, n_features], "state")
+        self.a = tf.placeholder(tf.float32, None, name="act")
+        self.td_error = tf.placeholder(tf.float32, None, name="td_error")  # TD_error
 
-        self._build_net()
-
-        self.sess = tf.Session()
-
-        if output_graph:
-            # $ tensorboard --logdir=logs
-            # http://0.0.0.0:6006/
-            # tf.train.SummaryWriter soon be deprecated, use following
-            tf.summary.FileWriter("logs/", self.sess.graph)
-
-        self.sess.run(tf.global_variables_initializer())
-
-    def _build_net(self):
-        with tf.name_scope('inputs'):
-            self.tf_obs = tf.placeholder(tf.float32, [None, self.n_features], name="observations")
-            self.tf_acts = tf.placeholder(tf.int32, [None, ], name="actions_num")
-            self.tf_vt = tf.placeholder(tf.float32, [None, ], name="actions_value")
-        # fc1
-        layer = tf.layers.dense(
-            inputs=self.tf_obs,
-            units=10,
-            activation=tf.nn.tanh,  # tanh activation
-            kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.3),
-            bias_initializer=tf.constant_initializer(0.1),
-            name='fc1'
-        )
-        # fc2
-        all_act = tf.layers.dense(
-            inputs=layer,
-            units=self.n_actions,
-            activation=None,
-            kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.3),
-            bias_initializer=tf.constant_initializer(0.1),
-            name='fc2'
+        l1 = tf.layers.dense(
+            inputs=self.s,
+            units=30,  # number of hidden units
+            activation=tf.nn.relu,
+            kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+            bias_initializer=tf.constant_initializer(0.1),  # biases
+            name='l1'
         )
 
-        self.all_act_prob = tf.nn.softmax(all_act, name='act_prob')  # use softmax to convert to probability
+        mu = tf.layers.dense(
+            inputs=l1,
+            units=1,  # number of hidden units
+            activation=tf.nn.tanh,
+            kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+            bias_initializer=tf.constant_initializer(0.1),  # biases
+            name='mu'
+        )
 
-        with tf.name_scope('loss'):
-            # to maximize total reward (log_p * R) is to minimize -(log_p * R), and the tf only have minimize(loss)
-            neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=all_act, labels=self.tf_acts)   # this is negative log of chosen action
-            # or in this way:
-            # neg_log_prob = tf.reduce_sum(-tf.log(self.all_act_prob)*tf.one_hot(self.tf_acts, self.n_actions), axis=1)
-            loss = tf.reduce_mean(neg_log_prob * self.tf_vt)  # reward guided loss
+        sigma = tf.layers.dense(
+            inputs=l1,
+            units=1,  # output units
+            activation=tf.nn.softplus,  # get action probabilities
+            kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+            bias_initializer=tf.constant_initializer(1.),  # biases
+            name='sigma'
+        )
+        global_step = tf.Variable(0, trainable=False)
+        # self.e = epsilon = tf.train.exponential_decay(2., global_step, 1000, 0.9)
+        self.mu, self.sigma = tf.squeeze(mu*2), tf.squeeze(sigma+0.1)
+        self.normal_dist = tf.distributions.Normal(self.mu, self.sigma)
+
+        self.action = tf.clip_by_value(self.normal_dist.sample(1), action_bound[0], action_bound[1])
+
+        with tf.name_scope('exp_v'):
+            log_prob = self.normal_dist.log_prob(self.a)  # loss without advantage
+            self.exp_v = log_prob * self.td_error  # advantage (TD_error) guided loss
+            # Add cross entropy cost to encourage exploration
+            self.exp_v += 0.01*self.normal_dist.entropy()
 
         with tf.name_scope('train'):
-            self.train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
+            self.train_op = tf.train.AdamOptimizer(lr).minimize(-self.exp_v, global_step)    # min(v) = max(-v)
 
-    def choose_action(self, observation):
-        prob_weights = self.sess.run(self.all_act_prob, feed_dict={self.tf_obs: observation[np.newaxis, :]})
-        action = np.random.choice(range(prob_weights.shape[1]), p=prob_weights.ravel())  # select action w.r.t the actions prob
-        return action
+    def learn(self, s, a, td):
+        s = s[np.newaxis, :]
+        feed_dict = {self.s: s, self.a: a, self.td_error: td}
+        _, exp_v = self.sess.run([self.train_op, self.exp_v], feed_dict)
+        return exp_v
 
-    def store_transition(self, s, a, r):
-        self.ep_obs.append(s)
-        self.ep_as.append(a)
-        self.ep_rs.append(r)
-
-    def learn(self):
-        # discount and normalize episode reward
-        discounted_ep_rs_norm = self._discount_and_norm_rewards()
-
-        # train on episode
-        self.sess.run(self.train_op, feed_dict={
-             self.tf_obs: np.vstack(self.ep_obs),  # shape=[None, n_obs]
-             self.tf_acts: np.array(self.ep_as),  # shape=[None, ]
-             self.tf_vt: discounted_ep_rs_norm,  # shape=[None, ]
-        })
-
-        self.ep_obs, self.ep_as, self.ep_rs = [], [], []    # empty episode data
-        return discounted_ep_rs_norm
-
-    def _discount_and_norm_rewards(self):
-        # discount episode rewards
-        discounted_ep_rs = np.zeros_like(self.ep_rs)
-        running_add = 0
-        for t in reversed(range(0, len(self.ep_rs))):
-            running_add = running_add * self.gamma + self.ep_rs[t]
-            discounted_ep_rs[t] = running_add
-
-        # normalize episode rewards
-        discounted_ep_rs -= np.mean(discounted_ep_rs)
-        discounted_ep_rs /= np.std(discounted_ep_rs)
-        return discounted_ep_rs
+    def choose_action(self, s):
+        s = s[np.newaxis, :]
+        return self.sess.run(self.action, {self.s: s})  # get probabilities for all actions
 
 
-DISPLAY_REWARD_THRESHOLD = 400  # renders environment if total episode reward is greater then this threshold
+class Critic(object):
+    def __init__(self, sess, n_features, lr=0.01):
+        self.sess = sess
+        with tf.name_scope('inputs'):
+            self.s = tf.placeholder(tf.float32, [1, n_features], "state")
+            self.v_ = tf.placeholder(tf.float32, [1, 1], name="v_next")
+            self.r = tf.placeholder(tf.float32, name='r')
+
+        with tf.variable_scope('Critic'):
+            l1 = tf.layers.dense(
+                inputs=self.s,
+                units=30,  # number of hidden units
+                activation=tf.nn.relu,
+                kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+                bias_initializer=tf.constant_initializer(0.1),  # biases
+                name='l1'
+            )
+
+            self.v = tf.layers.dense(
+                inputs=l1,
+                units=1,  # output units
+                activation=None,
+                kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+                bias_initializer=tf.constant_initializer(0.1),  # biases
+                name='V'
+            )
+
+        with tf.variable_scope('squared_TD_error'):
+            self.td_error = tf.reduce_mean(self.r + GAMMA * self.v_ - self.v)
+            self.loss = tf.square(self.td_error)    # TD_error = (r+gamma*V_next) - V_eval
+        with tf.variable_scope('train'):
+            self.train_op = tf.train.AdamOptimizer(lr).minimize(self.loss)
+
+    def learn(self, s, r, s_):
+        s, s_ = s[np.newaxis, :], s_[np.newaxis, :]
+
+        v_ = self.sess.run(self.v, {self.s: s_})
+        td_error, _ = self.sess.run([self.td_error, self.train_op],
+                                          {self.s: s, self.v_: v_, self.r: r})
+        return td_error
+
+
+OUTPUT_GRAPH = False
+MAX_EPISODE = 1000
+MAX_EP_STEPS = 200
+DISPLAY_REWARD_THRESHOLD = -100  # renders environment if total episode reward is greater then this threshold
 RENDER = False  # rendering wastes time
+GAMMA = 0.9
+LR_A = 0.001    # learning rate for actor
+LR_C = 0.01     # learning rate for critic
 
-env = gym.make('CartPole-v0')
-env.seed(1)     # reproducible, general Policy gradient has high variance
+env = gym.make('Pendulum-v0')
+env.seed(1)  # reproducible
 env = env.unwrapped
 
-print(env.action_space)
-print(env.observation_space)
-print(env.observation_space.high)
-print(env.observation_space.low)
+N_S = env.observation_space.shape[0]
+A_BOUND = env.action_space.high
 
-RL = PolicyGradient(
-    n_actions=env.action_space.n,
-    n_features=env.observation_space.shape[0],
-    learning_rate=0.02,
-    reward_decay=0.99,
-    # output_graph=True,
-)
+sess = tf.Session()
 
-for i_episode in range(3000):
+actor = Actor(sess, n_features=N_S, lr=LR_A, action_bound=[-A_BOUND, A_BOUND])
+critic = Critic(sess, n_features=N_S, lr=LR_C)
 
-    observation = env.reset()
+sess.run(tf.global_variables_initializer())
 
+if OUTPUT_GRAPH:
+    tf.summary.FileWriter("logs/", sess.graph)
+
+for i_episode in range(MAX_EPISODE):
+    s = env.reset()
+    t = 0
+    ep_rs = []
     while True:
-        if RENDER: env.render()
+        # if RENDER:
+        env.render()
+        a = actor.choose_action(s)
 
-        action = RL.choose_action(observation)
+        s_, r, done, info = env.step(a)
+        r /= 10
 
-        observation_, reward, done, info = env.step(action)
+        td_error = critic.learn(s, r, s_)  # gradient = grad[r + gamma * V(s_) - V(s)]
+        actor.learn(s, a, td_error)  # true_gradient = grad[logPi(s,a) * td_error]
 
-        RL.store_transition(observation, action, reward)
-
-        if done:
-            ep_rs_sum = sum(RL.ep_rs)
-
+        s = s_
+        t += 1
+        ep_rs.append(r)
+        if t > MAX_EP_STEPS:
+            ep_rs_sum = sum(ep_rs)
             if 'running_reward' not in globals():
                 running_reward = ep_rs_sum
             else:
-                running_reward = running_reward * 0.99 + ep_rs_sum * 0.01
-            if running_reward > DISPLAY_REWARD_THRESHOLD: RENDER = True     # rendering
+                running_reward = running_reward * 0.9 + ep_rs_sum * 0.1
+            if running_reward > DISPLAY_REWARD_THRESHOLD: RENDER = True  # rendering
             print("episode:", i_episode, "  reward:", int(running_reward))
-
-            vt = RL.learn()
-
-            if i_episode == 0:
-                plt.plot(vt)    # plot the episode vt
-                plt.xlabel('episode steps')
-                plt.ylabel('normalized state-action value')
-                plt.show()
             break
-
-        observation = observation_
